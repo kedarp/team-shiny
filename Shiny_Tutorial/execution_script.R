869c33a945c718e8ef18210b3e2b326925a59c6d
kedarp <kedar.rp@gmail.com>
2015-03-05 07:17:22 -0500

    tutorial lesson1

---
+++ Shiny_Tutorial/execution_script.R
@@ -0,0 +1,21 @@
+library("shiny")
+getwd()
+setwd("/Users/kedarpatil/Documents/St-4701 - Data Visualization/team-shiny/Shiny_Tutorial")
+runApp("old_faithful")
+runApp("old_faithful", display.mode="showcase")
+
+#GO FURTHER - LESSON1
+
+system.file("examples", package="shiny")
+
+runExample("01_hello") # a histogram
+runExample("02_text") # tables and data frames
+runExample("03_reactivity") # a reactive expression
+runExample("04_mpg") # global variables
+runExample("05_sliders") # slider bars
+runExample("06_tabsets") # tabbed panels
+runExample("07_widgets") # help text and submit buttons
+runExample("08_html") # Shiny app built from HTML
+runExample("09_upload") # file upload wizard
+runExample("10_download") # file download wizard
+runExample("11_timer") # an automated timer
