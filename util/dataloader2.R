1fc3b238a444a72461edbea23ef5b1ef4215495a
Hafiz Ahsan <hafiz@dyn-129-236-230-33.dyn.columbia.edu>
2015-04-21 20:34:26 -0400

    bugfix

--- util/dataloader2.R
+++ util/dataloader2.R
@@ -3,7 +3,7 @@ loadFatalityDataset <- function(year) {
     zipFileName <- paste('data/NHTSA_Fatality-', year, '.zip', sep='')
     ds$accidents <- read.csv(unz(zipFileName, 'Accident-2013.csv'))
     ds$persons <- read.csv(unz(zipFileName, 'Person-2013.csv'))
-    ds$vehicles <- read.csv(unz(zipFileName, 'Person-2013.csv'))
+    ds$vehicles <- read.csv(unz(zipFileName, 'Vehicle-2013.csv'))
     ds$states <- read.csv('data/states.csv')
     ds$avm <- read.csv(paste('data/annual-vehicle-miles-', year, '.csv', sep=''))
     ds$urbanPct <- read.csv(paste('data/urban-percent-', year, '.csv', sep=''))
