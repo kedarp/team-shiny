7850090779a2ad283e095ab8c0365e89456de2a2
Dennis Lyubyvy <dennis.lyubyvy@gmail.com>
2015-04-22 10:58:14 -0400

    animated map!!!

---
+++ shiny_propotype/v4/ui.R
@@ -0,0 +1,166 @@
+library(shinydashboard)
+
+source('../../util/dataloader2.R')
+
+dashboardPage(
+  dashboardHeader(
+    title = "US Roads Fatality Data"),
+  dashboardSidebar(
+    sidebarMenu(
+        menuItem("General summary", tabName="summary_tab", icon=icon("car")),
+        menuItem("Timing", tabName="timing_tab", icon=icon("calendar")),
+        menuItem("Alcohol", tabName="alco_tab", icon=icon("beer")),
+        menuItem("Children", tabName="children_tab", icon=icon("child")),
+        menuItem("Bikes", tabName="bike_tab", icon=icon("bicycle")),
+        menuItem("Motorcycles", tabName="moto_tab", icon=icon("bolt")),
+        menuItem("Trends", tabName="trends_tab", icon=icon("line-chart")),
+        menuItem("Largest crashes", tabName="largest_tab", icon=icon("users")),
+        menuItem("Map", tabName="map_tab", icon=icon("map-marker"))
+        )
+    ),
+  dashboardBody(
+
+####################### 
+tabItems(
+    tabItem(tabName="summary_tab",
+        # Boxes need to be put in a row (or column)
+        fluidRow(
+            column(width=12, 
+                   h2("General summary"),
+                   p("by state"),
+                   p("by number of fatalities"),
+                   p("by type of vehicle"),
+                   p("Rollover fatalities"),
+                   p("Fatalities vs Restraint")
+            )
+        )
+    ),
+      
+####################### 
+    tabItem(tabName="timing_tab",
+            # Boxes need to be put in a row (or column)
+            fluidRow(
+                column(width=12),
+                h3('All 2013 Fatalities by weekday'),
+                plotOutput("timingPlot1"),
+                h3('All 2013 Fatalities by weekday and State'),
+                plotOutput("timingPlot2"),
+                h3('Time of day'),
+                plotOutput("timingPlot3"),
+                h3('Over the course of the week'),
+                plotOutput("timingPlot4")
+            )
+    ),
+    
+####################### 
+  tabItem(tabName="alco_tab",
+          # Boxes need to be put in a row (or column)
+          fluidRow(
+              column(width=12, 
+                     h2("Alcohol"),
+                     p("Change over the years"),
+                     p("% of total fatalities over the years"),
+                     p("Do we have peaks on national holidays"),
+                     p("City/Intercity"),
+                     p(""))
+          )
+  ),
+  
+              
+
+
+####################### 
+  tabItem(tabName="children_tab",
+          # Boxes need to be put in a row (or column)
+          fluidRow(
+              column(width=12, 
+                     h2("Children"),
+                     p("% by state"),
+                     p("% by drunk drivers"),
+                     p("% by city/intercity road"),
+                     p(""),
+                     p(""))
+          )
+  ),
+  
+                  
+####################### 
+  tabItem(tabName="bike_tab",
+          # Boxes need to be put in a row (or column)
+          fluidRow(
+              column(width=12, 
+                     h2("Bikes"),
+                     p("% by year"),
+                     p("% by state"),
+                     p(""),
+                     p(""),
+                     p(""))
+          )
+  ),
+                   
+                      
+                      
+####################### 
+  tabItem(tabName="moto_tab",
+          # Boxes need to be put in a row (or column)
+          fluidRow(
+              column(width=12, 
+                     h2("Motorcycles"),
+                     p("% by year"),
+                     p("% by state"),
+                     p(""),
+                     p(""),
+                     p(""))
+          )
+  ),
+####################### 
+    tabItem(tabName="trends_tab",
+            # Boxes need to be put in a row (or column)
+            fluidRow(
+                column(width=12, 
+                       h2("Trends"),
+                       p("Seasonality by state"),
+                       p("Average seasonality"),
+                       p("Average seasonality"),
+                       p("By total number of fatalities (seasonal trends)"),
+                       p("Fatalities vs weekday"),
+                       p("Fatalities by type of the vehicle"),
+                       p(""),
+                       p(""))
+            )
+    ),
+                              
+                              
+                              
+####################### 
+  tabItem(tabName="largest_tab",
+          # Boxes need to be put in a row (or column)
+          fluidRow(
+              column(width=12, 
+                     h2("Largest crashes"),
+                     p("Biggest accidents overall"),
+                     p("Biggest accidents by state"),
+                     p("Biggest accidents with bikes"),
+                     p(""),
+                     p(""))
+          )
+  ),
+  
+                                  
+                                  
+####################### 
+  tabItem(tabName="map_tab",
+          # Boxes need to be put in a row (or column)
+          fluidRow(
+              column(width=12, 
+                     h2("Map"),
+                     p("Animated map of US roads collisions in 2013:"),
+                     tags$iframe(src = "https://denniskorablev.cartodb.com/viz/e2b8cbac-e8fe-11e4-8cab-0e853d047bba/embed_map", seamless=NA,width='100%',height='520'),
+                     p(""),
+                     p(""))
+          )
+  )
+          
+      )
+      )
+  )
